40d6c889f5c6e46c3c49cfc781d10977c53a43dd
jithu5 <abijithr202@gmail.com>
2025-01-22 10:02:43 +0530

    implement high score tracking for current game

--- score_board.py
+++ score_board.py
@@ -5,6 +5,7 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = 0
         self.penup()
         self.hideturtle()
         self.goto(-80,260)
@@ -13,13 +14,14 @@ class Scoreboard(Turtle):
 
     def update_score(self):
         self.clear()
-        self.write(f"Score : {self.score}", move=False, align="left", font=('Arial', 24, 'normal'))
+        self.write(f"Score : {self.score} High Score : {self.high_score}", move=False, align="left", font=('Arial', 24, 'normal'))
 
     def increase_score(self):
         self.score += 1
         self.update_score()
 
-    def game_over(self):
-        self.goto(0,0)
-        self.write("Game Over!", move=False, align="center", font=('Arial', 24, 'normal'))
-        self.hideturtle()
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.update_score()
